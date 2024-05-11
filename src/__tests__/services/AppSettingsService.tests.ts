6957e5bf7299f2610d8d4d39bf7b270aee890db2
Michael Vastarelli <mike@bitcobblers.com>
2024-05-11 17:42:34 -0400

    test: partially ported tests

---
+++ src/__tests__/services/AppSettingsService.tests.ts
@@ -0,0 +1,105 @@
+import * as settings from '../../services/AppSettingsService';
+import { settingsApi } from '../../shared/SettingsApi';
+import { Wallet, Coin, Miner, AppSettings } from '../../models';
+
+describe('App Settings Service', () => {
+  describe('Wallets tests', () => {
+    it('Returns stored content', async () => {
+      // Arrange.
+      const wallets: Wallet[] = [
+        {
+          id: '12345',
+          name: 'test',
+          network: 'ETH',
+          address: '0x12345',
+          memo: '',
+        },
+      ];
+
+      jest.spyOn(settingsApi, 'read').mockReturnValue(Promise.resolve(JSON.stringify(wallets)));
+
+      // Act.
+      const result = await settings.getWallets();
+
+      // Assert.
+      expect(result).toStrictEqual(wallets);
+    });
+  });
+
+  describe('Coins tests', () => {
+    it('Returns stored content', async () => {
+      // Arrange.
+      const coins: Coin[] = [
+        {
+          symbol: 'ETH',
+          wallet: 'wallet',
+          enabled: true,
+          duration: 1,
+        },
+      ];
+
+      jest.spyOn(settingsApi, 'read').mockReturnValue(Promise.resolve(JSON.stringify(coins)));
+
+      // Act.
+      const result = await settings.getCoins();
+
+      // Assert.
+      expect(result).toStrictEqual(coins);
+    });
+  });
+
+  describe('Miners tests', () => {
+    it('Returns stored content', async () => {
+      // Arrange.
+      const miners: Miner[] = [
+        {
+          id: 'x',
+          kind: 'lolminer',
+          name: 'miner',
+          version: '',
+          algorithm: 'etchash',
+          parameters: '',
+        },
+      ];
+
+      jest.spyOn(settingsApi, 'read').mockReturnValue(Promise.resolve(JSON.stringify(miners)));
+
+      // Act.
+      const result = await settings.getMiners();
+
+      // Assert.
+      expect(result).toStrictEqual(miners);
+    });
+  });
+
+  describe('Settings tests', () => {
+    it('Returns stored content', async () => {
+      // Arrange.
+      const appSettings: AppSettings = {
+        settings: {
+          workerName: 'worker',
+          defaultMiner: '',
+          coinStrategy: 'normal',
+          proxy: '',
+        },
+        pools: {
+          etchash: '',
+          kawpow: '',
+          autolykos2: '',
+          randomx: '',
+        },
+        appearance: {
+          theme: 'light',
+        },
+      };
+
+      jest.spyOn(settingsApi, 'read').mockReturnValue(Promise.resolve(JSON.stringify(appSettings)));
+
+      // Act.
+      const result = await settings.getAppSettings();
+
+      // Assert.
+      expect(result).toStrictEqual(appSettings);
+    });
+  });
+});
---
+++ src/__tests__/services/Formatters.tests.ts
@@ -0,0 +1,97 @@
+import * as formatter from '../../services/Formatters';
+
+describe('Formatters Service', () => {
+  describe('Hashrate For Empty Values', () => {
+    const cases = [
+      null,
+      undefined,
+    ];
+
+    test.each(cases)('%p', (given) => {
+      // Act.
+      const result = formatter.hashrate(given);
+
+      // Assert.
+      expect(result).toBe('N/A');
+    });
+  });
+
+  describe('Hashrate Formatting', () => {
+    it('Should return raw number if no scale provided.', () => {
+      // Act.
+      const result = formatter.hashrate(10);
+
+      // Assert.
+      expect(result).toBe('10');
+    });
+
+    it('Should return unaltered suffix for M', () => {
+      // Act.
+      const result = formatter.hashrate(10, 'M');
+
+      // Assert.
+      expect(result).toBe('10MH/s');
+    });
+
+    it('Should return scaled number scaled by 1000 for K', () => {
+      // Act.
+      const result = formatter.hashrate(1000, 'K');
+
+      // Assert.
+      expect(result).toBe('1KH/s');
+    });
+  });
+
+  describe('Uptime', () => {
+    const cases = [
+      { expected: 'N/A' },
+      { given: 1, expected: '1s' },
+      { given: 1.5, expected: '1s' },
+      { given: 60, expected: '1min' },
+      { given: 60.5, expected: '1min' },
+      { given: 90, expected: '1min 30s' },
+      { given: 3600, expected: '1hr' },
+      { given: 3600.5, expected: '1hr' },
+      { given: 3601, expected: '1hr 1s' },
+      { given: 3660, expected: '1hr 1min' },
+      { given: 3690, expected: '1hr 1min 30s' },
+      { given: 86400, expected: '1d' },
+      { given: 86400.5, expected: '1d' },
+      { given: 86401, expected: '1d 1s' },
+      { given: 86460, expected: '1d 1min' },
+      { given: 86490, expected: '1d 1min 30s' },
+      { given: 90000, expected: '1d 1hr' },
+      { given: 90001, expected: '1d 1hr 1s' },
+      { given: 90060, expected: '1d 1hr 1min' },
+      { given: 90090, expected: '1d 1hr 1min 30s' },
+    ];
+
+    test.each(cases)('%p', ({ given, expected }) => {
+      // Act.
+      const result = formatter.uptime(given);
+
+      // Assert.
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('Duration', () => {
+    const cases = [
+      { expected: 'N/A' },
+      { given: 1, expected: '1hr' },
+      { given: 24, expected: '1d' },
+      { given: 30, expected: '1d 6hr' },
+      { given: 168, expected: '1w' },
+      { given: 192, expected: '1w 1d' },
+      { given: 193, expected: '1w 1d 1hr' },
+    ];
+
+    test.each(cases)('%p', ({ given, expected }) => {
+      // Act.
+      const result = formatter.duration(given);
+
+      // Assert.
+      expect(result).toBe(expected);
+    });
+  });
+});
