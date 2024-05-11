4af8de9fde57d54531cf67e30da6ed180077d22c
Michael Vastarelli <mike@bitcobblers.com>
2024-05-11 18:03:26 -0400

    chore: deleted unused interface.

--- src/shared/AboutApi.ts
+++ src/shared/AboutApi.ts
@@ -1,11 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { invoke } from '@tauri-apps/api';
 
-export interface AboutApi {
-  getName: () => Promise<string>;
-  getVersion: () => Promise<string>;
-}
-
 export const aboutApi = {
   getName: () => invoke<string>('about_get_name'),
   getVersion: () => invoke<string>('about_get_version'),
