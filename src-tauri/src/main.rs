6589f50194b96e351afae4ab726dd57088b56a3e
Michael Vastarelli <mike@bitcobblers.com>
2024-05-16 17:17:05 -0400

    chore: removed demo command.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,9 @@
 
 mod about;
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            greet,
             about::about_get_name,
             about::about_get_version,
             about::about_open_browser,
